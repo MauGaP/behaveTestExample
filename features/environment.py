779f1d8534b3a5803e116805d434c22fe6e46451
MauGaP <mauro.pedano@gmail.com>
2022-02-08 00:53:11 -0300

    updated screenshot folder with relative path

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,5 @@
-import time
-
-
 # capture screenshot of failed tests
 def after_step(context, step):
     if step.status == 'failed':
         step_name = step.name
-        context.driver.save_screenshot('C:/Repos/behaveTestExample/features/screenshots/failed-' + step_name + '.png')
+        context.driver.save_screenshot('/features/screenshots/failed-' + step_name + '.png')
