533a91dcd851fe316e1b2340d3ef3b9c05cb84e1
mickaela-a <alarconmickaela@gmail.com>
2022-07-28 20:35:18 -0300

    new file

--- features/page_objects/BasePage.py
+++ features/page_objects/BasePage.py
@@ -17,24 +17,3 @@ def wait_until_element_displayed(self, locator):
 
 def wait_until_url_changes(self):
     WebDriverWait(self.driver, 2).until(expected_conditions.url_changes)
-<<<<<<< HEAD
-
-
-def wait_until_alert_is_present(self):
-    WebDriverWait(self.driver, 2).until(expected_conditions.alert_is_present())
-
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-
-driver = webdriver.Chrome('C:/Repos/behaveTestExample/drivers/chromedriver.exe')
-
-def click_elemrnt_by_css_selector(selector) :
-    driver.find_element(By.CSS_SELECTOR, selector).click()
-
-def go_to_website(url) :
-    driver.get(url)
-
-def llenar_campo(selector, string) :
-    driver.find_element(By.CSS_SELECTOR, selector).send_keys(string)
-=======
->>>>>>> 0dcbc8f40d049594056217fe0b41570675bb96e9
