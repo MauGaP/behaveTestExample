f975344be0c8e3ff88acaee1bc5b403e9a3dcfd6
Msubils <maximiliano.subils@gmail.com>
2022-08-16 20:26:46 -0300

    Remove item from cart

--- features/steps/cart_steps.py
+++ features/steps/cart_steps.py
@@ -1,6 +1,8 @@
+from behave import then, when
 from features.page_objects.compra_page import BOTON_CHECKOUT, BOTON_CONTINUE, BOTON_FINISH, FIRST_NAME, LAST_NAME, MENSAJE_COMPRA_EXITOSA, POSTAL_CODE, VALID_FIRST_NAME, VALID_LAST_NAME, VALID_POSTAL_CODE
 from page_objects.BasePage import click_element_by_css_selector, send_keys_by_css_selector
 from features.page_objects.BasePage import get_element_text
+import time
 
 @when('el usuario completa la compra')
 def generate_purchase(context) :
@@ -10,6 +12,10 @@ def generate_purchase(context) :
     send_keys_by_css_selector(context, POSTAL_CODE, VALID_POSTAL_CODE)
     click_element_by_css_selector(context, BOTON_CONTINUE)
     click_element_by_css_selector(context, BOTON_FINISH)
+
+@when('the user removes an item from the cart')
+def remove_item(context) :
+    click_element_by_css_selector(context, '#remove-sauce-labs-backpack')
 
 @then('se muestra el mensaje de compra exitosa')
 def SUCCESSFUL_PURCHASE_MESSAGE(context) :
--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -5,7 +5,7 @@ from features.page_objects.inventory_page import ADD_BACKPACK_TO_CART_BTN, CART_
 from page_objects.BasePage import click_element_by_css_selector
 import time
 
-@given('el usuario tiene un producto en el carrito')
+@given('the user has an item in the cart')
 def product_in_cart(context) :
     click_element_by_css_selector(context, ADD_BACKPACK_TO_CART_BTN)
     click_element_by_css_selector(context, CART_BTN)
